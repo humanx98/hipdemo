1e51ca262bcbd799179f173840489b70d410be1b
Bohdan Semenov <bodyasemzen@gmail.com>
2024-12-13 18:03:28 +0200

    fix reading hip uuid for linux

--- src/hip/common.c
+++ src/hip/common.c
@@ -52,12 +52,29 @@ b8 hip_print_devices_and_get_count(u32* result_device_count) {
 b8 hip_get_device_uuid(u32 device_id, HipUUID* result) {
     assert(result);
 
+#if _WIN64
     hipUUID uuid;
-    if(HIP_FAILED(hipDeviceGetUuid(&uuid, device_id))) {
+    if (HIP_FAILED(hipDeviceGetUuid(&uuid, device_id))) {
+        return false;
+    }
+    WW_STATIC_ASSERT_EXPR(sizeof(result->bytes) == sizeof(uuid.bytes), "");
+    memcpy(result->bytes, uuid.bytes, sizeof(result->bytes));
+#else
+    // The value that hipDeviceGetUuid returns does not correspond with those returned
+    // by mesa (see https://gitlab.freedesktop.org/mesa/mesa/-/blob/5cd3e395037250946ba2519600836341df02c8ca/src/amd/common/ac_gpu_info.c#L1366-1382)
+    // and by xgl (see https://github.com/GPUOpen-Drivers/xgl/blob/4118707939c2f4783d28ce2a383184a3794ca477/icd/api/vk_physical_device.cpp#L4363-L4421)
+    // Those drivers _do_ align with each other, so we can create our own UUID here.
+    // \see https://github.com/ROCm-Developer-Tools/hipamd/issues/50.
+    hipDeviceProp_t props;
+    if (HIP_FAILED(hipGetDeviceProperties(&props, device_id))) {
         return false;
     }
 
-    WW_STATIC_ASSERT_EXPR(sizeof(result->bytes) == sizeof(uuid.bytes), "");
-    memcpy(result->bytes, uuid.bytes, sizeof(result->bytes));
+    memset(result->bytes, 0, sizeof(result->bytes));
+    u32* uuid_ints = (u32*)result->bytes;
+    uuid_ints[0] = props.pciDomainID;
+    uuid_ints[1] = props.pciBusID;
+    uuid_ints[2] = props.pciDeviceID;
+#endif
     return true;
 }
